664bb3f55d1ccdf1fb36511f6ecd68a646f99333
Must-Be-Mad <136653460+Must-Be-Mad@users.noreply.github.com>
2024-08-27 13:55:37 +0100

    Update Cryptic-Links.js

--- Game/Cryptic-Links.js
+++ Game/Cryptic-Links.js
@@ -48,7 +48,7 @@ const cluelist =[["Transporting a letter", "To play a charge", "A holy man befor
 ["A holy man that has a foot fetish","Swifty that doesn't like red","Doubling something","To estimate a golf stand"],
 ["Between a bell and a circle","Space for a note","Mother's identification","A closeby sound"],
 ["Hideout is myself being","When a sorcerer comes in","Heard that was with Johnny","Education of adult movies"],
-["A rythm with frizzy hair","A backwards part","Initially pretty overpowered","To ask questions to vehicles"],
+["A rhythm with frizzy hair","A backwards part","Initially pretty overpowered","To ask questions to vehicles"],
 ["Melt thrice and EE","L=2n","Nickel and Neon","Take the head of fight down a notch"],
 ["Mardi buyer","Approach got uninstalled","Pooh bear's favourite food simply is","Fake hear for your audio receptors"],
 ["Untangle cheeps","Sounds like someone got drenched from the mains","Adressing a saint","Three Calvin Kleins"]
